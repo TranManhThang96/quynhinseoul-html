b2a8907c86d22fc8fecc46b1f71fa3704d111034
thangtm <thang.tran@uptempo-global.com>
2024-03-13 17:55:03 +0700

    splide

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import './style.css';
 import './styles/home.scss';
+
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+new Splide('.splide').mount();
+
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
