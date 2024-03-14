c055f6d3003217e648335a795bc0a7ee30a1b865
thangtm <thang.tran@uptempo-global.com>
2024-03-14 17:14:49 +0700

    updatre

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import './styles/home.scss';
 
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
-new Splide('.splide').mount();
+var splide = new Splide('.splide', {
+  autoplay: true,
+  interval: 3000,
+  perPage: 1,
+  rewind: true,
+});
+
+splide.mount();
 
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
