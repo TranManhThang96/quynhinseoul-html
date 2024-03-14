1575228cfdeccbb02442d2626e4576495dd746cf
DESKTOP-NC36HMS\thangtm <tranmanhthang96@gmail.com>
2024-03-14 21:57:07 +0700

    marquee

--- main.js
+++ main.js
@@ -3,14 +3,41 @@ import './styles/home.scss';
 
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
+import marquee from 'vanilla-marquee';
+
 var splide = new Splide('.splide', {
   autoplay: true,
   interval: 3000,
   perPage: 1,
   rewind: true,
 });
-
 splide.mount();
 
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
+document.addEventListener('DOMContentLoaded', () => {
+  const marquee1 = new marquee(document.getElementById('marquee1'), {
+    delayBeforeStart: 0,
+    css3easing: 'ease-in',
+    direction: 'down',
+    duplicated: true,
+    gap: 0,
+    duration: 4000000,
+    recalcResize: true,
+  });
+
+  const marquee2 = new marquee(document.getElementById('marquee2'), {
+    delayBeforeStart: 0,
+    css3easing: 'ease-in',
+    direction: 'up',
+    duplicated: true,
+    gap: 0,
+    duration: 4000000,
+    recalcResize: true,
+  });
+
+  setTimeout(() => {
+    marquee1.pause();
+    marquee2.pause();
+    marquee1.destroy();
+    marquee2.destroy();
+  }, 50000);
+});
